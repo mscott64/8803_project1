47970af48b4787057ec7df75355a8b30933896e1
Michelle Scott <mscott64@gatech.edu>
2012-09-29 19:59:32 +0000

    Started to set up client framework

---
+++ client.c
@@ -0,0 +1,14 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void* client_create(void *data)
+{
+  printf("Client says hello\n");
+  return NULL;
+}
---
+++ client.h
@@ -0,0 +1,6 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+void *client_create(void *data);
+
+#endif /* CLIENT_H */
